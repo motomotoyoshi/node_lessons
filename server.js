5da07859042bbbbbe82df55c4aa7d56ed1c7b547
motomotoyoshi <motomoto.git@gmail.com>
2019-10-05 20:01:03 +0900

    create, hello.html

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 'use strict';
 
 const setting = require('./setting');
+const fs = require('fs');
 const http = require('http');
 const server = http.createServer();
-let msg;
 server.on('request', function (req, res) {
-    switch (req.url) {
-        case '/about':
-            msg = 'about this page';
-            break;
-        case '/profile':
-            msg = 'about me';
-            break;
-        default:
-            msg = 'wrong page';
-            break;
-    }
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(msg);
-    res.end();
-})
+    fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function (err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.write('Not found');
+            return res.end();
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    })
+});
 server.listen(setting.port, setting.host);
 console.log('server listening ...');
