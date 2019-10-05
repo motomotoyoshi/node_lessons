a923e3e7ed4c21857ff4af110a1524ce7f48261c
motomotoyoshi <motomoto.git@gmail.com>
2019-10-05 18:28:27 +0900

    update, server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 'use strict';
 
 const setting = require('./setting');
-console.log(setting);
 const http = require('http');
 const server = http.createServer();
+let msg;
 server.on('request', function (req, res) {
+    switch (req.url) {
+        case '/about':
+            msg = 'about this page';
+            break;
+        case '/profile':
+            msg = 'about me';
+            break;
+        default:
+            msg = 'wrong page';
+            break;
+    }
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello World');
+    res.write(msg);
     res.end();
 })
 server.listen(setting.port, setting.host);
